2b7c5f2363d256e58086a47c14b71b5e99ba89bc
liis17 <daskent96@gmail.com>
2024-11-02 22:49:14 +0300

    Переписана програама , добавлены новые параметры запуска

--- PassGen/Program.cs
+++ PassGen/Program.cs
@@ -1,59 +1,124 @@
-﻿namespace PassGen
+﻿using System;
+using System.Text;
+
+namespace PassGen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var value = 1;
-            var stringpass = "";
-            var inparam = "";
-            var outputpass = true;
-            foreach (string arg in args)
-            {
-                inparam += arg;
-            }
-            if (inparam == "")
-            {
-                //bruh
-            }
-            else if (inparam.Contains("-d1") || inparam.Contains("-d2") || inparam.Contains("-d3") || inparam.Contains("-dif1") || inparam.Contains("-dif2") || inparam.Contains("-dif3"))
-            {
-                var param = inparam.Replace("-d", "").Replace("-dif", "");
+            int passwordLength = 20; // длина пароля по умолчанию
+            int passwordCount = 1;  // количество паролей по умолчанию
+            int complexityLevel = 3; // уровень сложности по умолчанию
 
-                try
+            int version = 2; // текущая версия программы
+
+            Console.WriteLine($"        PASSGEN by Li_is v{version}");
+            Console.WriteLine();
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Программа имеет дополнительнрые параметры.\nИспользуйте -h или -help для справки.", Console.ForegroundColor = ConsoleColor.DarkGray);
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+
+            
+
+            // Парсинг аргументов командной строки
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
                 {
-                    value = int.Parse(param);
+                    case "-h":
+                    case "-help":
+                        ShowHelp();
+                        break;
+
+                    case "-d":
+                    case "-dif":
+                        if (i + 1 < args.Length && int.TryParse(args[i + 1], out int level) && level >= 1 && level <= 3)
+                        {
+                            complexityLevel = level;
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: для -d или -dif необходимо указать значение от 1 до 3.");
+                        }
+                        break;
+
+                    case "-n":
+                    case "-num":
+                        if (i + 1 < args.Length && int.TryParse(args[i + 1], out int count) && count > 0)
+                        {
+                            passwordCount = count;
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: для -n или -num необходимо указать положительное число.");
+                        }
+                        break;
+
+                    case "-l":
+                    case "-len":
+                        if (i + 1 < args.Length && int.TryParse(args[i + 1], out int length) && length > 0)
+                        {
+                            passwordLength = length;
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка: для -l или -len необходимо указать положительное число.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine($"Неизвестный параметр: {args[i]}. Используйте -h или -help для справки.");
+                        break;
                 }
-                catch
+            }
+
+            GenerateAndDisplayPasswords(passwordLength, passwordCount, complexityLevel);
+            Console.WriteLine();
+            Console.WriteLine("Любая клавиша для выхода.");
+            Console.ReadKey();
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("------------- ПОМОЩЬ -------------");
+            Console.WriteLine("-h или -help         : Показать это сообщение.");
+            Console.WriteLine("-d или -dif [1-3]    : Уровень сложности пароля (1 - цифры, 2 - цифры и буквы, 3 - цифры, буквы и символы).");
+            Console.WriteLine("-n или -num [число]  : Задает количество паролей.");
+            Console.WriteLine("-l или -len [число]  : Задает длину пароля.");
+            Console.WriteLine("----------------------------------");
+        }
+
+        static void GenerateAndDisplayPasswords(int length, int count, int complexity)
+        {
+            string chars = complexity switch
+            {
+                1 => "0123456789",
+                2 => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+                3 => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()",
+                _ => "0123456789"
+            };
+
+            Random random = new();
+            for (int i = 0; i < count; i++)
+            {
+                StringBuilder password = new();
+                for (int j = 0; j < length; j++)
                 {
-                    Console.WriteLine("Неизвестный параметр для -d | -dif", Console.ForegroundColor = ConsoleColor.Red);
-                    outputpass = false;
+                    password.Append(chars[random.Next(chars.Length)]);
                 }
-            }
-            else if (inparam.Contains("-h") || inparam.Contains("-help"))
-            {
-                Console.WriteLine("-------------ПОМОЩЬ-------------", Console.ForegroundColor = ConsoleColor.DarkRed);
-                Console.WriteLine("-d [значение] | -dif [значение] - сложность, входное значение от 1 до 3, 1 по умолчанию", Console.ForegroundColor = ConsoleColor.White);
-                Console.WriteLine("-h | -help - помощь", Console.ForegroundColor = ConsoleColor.White);
-                Console.WriteLine("--------------------------------", Console.ForegroundColor = ConsoleColor.DarkRed);
-                outputpass = false;
-            }
-            else
-            {
-                Console.WriteLine("Неизвестный параметр запуска", Console.ForegroundColor = ConsoleColor.Red);
-                outputpass = false;
-            }
-            for (int i = 0; i < value; i++)
-            {
-                stringpass += Guid.NewGuid().ToString("N");
-            }
-            if (outputpass)
-            {
+
                 Console.Write(">>>   ", Console.ForegroundColor = ConsoleColor.DarkYellow);
-                Console.Write(stringpass, Console.ForegroundColor = ConsoleColor.Green);
-                Console.Write("   <<<", Console.ForegroundColor = ConsoleColor.DarkYellow);
+                Console.Write(password.ToString(), Console.ForegroundColor = ConsoleColor.Green);
+                Console.WriteLine("   <<<", Console.ForegroundColor = ConsoleColor.DarkYellow);
             }
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ResetColor();
         }
     }
 }
